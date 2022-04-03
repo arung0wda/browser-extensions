af9cc5044ff75b071ee8ad9f5a396989c3e0d5ca
arp <arp@akamai.com>
2022-04-03 12:30:03 +0530

    Init

---
+++ chartInkToTradingView/contentScript.js
@@ -0,0 +1,39 @@
+var screenerListLoadWatcher = setInterval (updateLinks, 1000);
+
+
+function updateLinks(){
+    console.log("updateLinks called.");
+
+    rows = $(".scan_results_table tr");
+    $($(rows[0]).children()[2]).text("TradingView"); //Update column header
+   
+    header = $(".scan_results_table tr")[0];
+
+
+    if(rows.length > 1){
+        for(i=1;i<rows.length;i++){
+            clearInterval (screenerListLoadWatcher);
+            col = $($($(rows[i]).children()[2]).children());
+            symbol = col.text();
+            tradingViewLink = 'https://in.tradingview.com/chart/?symbol='+symbol;
+            col.attr("href", tradingViewLink);
+            //console.log(tradingViewLink);
+        }
+    }
+}
+
+$(document).ready(function() {
+    $(".run_scan_button").click(function(){
+       console.log("Run scan clicked.");
+       screenerListLoadWatcher = setInterval (updateLinks, 5000);
+    }); 
+});
+
+$(document).ready(function() {
+    $(".paginate_button").click(function(e){ //Not working properly yet. TODO: update
+        console.log("Paginate clicked.");
+       screenerListLoadWatcher = setInterval (updateLinks, 1000);
+    }); 
+});
+
+ 
